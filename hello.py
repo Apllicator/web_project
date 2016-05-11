548bf655a48c05762369b4fc2f8e6642d6b6d0c4
Apllicator <Apllicator@yandex.ru>
2016-05-11 21:20:52 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
     status = '200'
     header = [('Content-Type', 'text/plain')]
-    status_response(status, header)
+    start_response(status, header)
     body = environ.replace('&', '\n')
     return [body]
