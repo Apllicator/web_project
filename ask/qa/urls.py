9d38d4ee60fbdd6a1c40854848c76283be12daa9
Apllicator <Apllicator@yandex.ru>
2016-05-15 00:08:00 +0300

    1

---
+++ ask/qa/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from qa.views import test
+
+urlpatterns = [
+	url(r'^$', test),
+	url(r'login/', test),
+	url(r'signup/', test),
+	url(r'ask/', test),
+	url(r'popular/', test),
+	url(r'new/', test),
+	url(r'question/?P<pk\d+', test),
+]
