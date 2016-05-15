4c978c0a5b2a5b537e92b81ef88879071a414e26
Apllicator <Apllicator@yandex.ru>
2016-05-15 10:36:14 +0000

    1

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -11,7 +11,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/home/box/web/ask')
+#sys.path.append('/home/box/web/ask')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 application = get_wsgi_application()
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from qa.views import test
 
-urlpatterns = patterns('qa.views',
-    url(r'^$', 'test'),
-    url(r'^login/.*$', 'test'),
-    url(r'^signup/.*', 'test'),
-    url(r'^question/(?P<id>[0-9]+)/$', test),
-    url(r'^ask/.*', 'test'),
-    url(r'^popular/.*', 'test'),
-    url(r'^new/.*', 'test'),
-)
+urlpatterns = [
+    url(r'^$', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^question/(?P<id>[0-9]+)/', test),
+    url(r'^ask/', test),
+    url(r'^popular/', test),
+    url(r'^new/', test),
+]
